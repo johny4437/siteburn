cad4cb491a4a63600f4141ae2ed44cfa3b350dc2
johny anastacio <johnyanastaciods@gmail.com>
2020-04-21 21:10:07 -0300

    site was finished

--- site/src/Site.js
+++ site/src/Site.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import Contact from './components/Contact';
 import Portfolio from   './components/Portfolio';
 import About from   './components/About';
+import ReactPixel from 'react-facebook-pixel';
 
 
 import './assets/css/bootstrap.min.css';
@@ -13,6 +14,14 @@ import './assets/css/agency.min.css';
 
 
 export default function Site() {
+    const advancedMatching = { em: 'burnbrainje@gmail.com' };
+    const options = {
+        autiConfig: true,
+        debug:false
+    };
+
+    ReactPixel.init('224347831980798', advancedMatching, options);
+    ReactPixel.pageView();
     return (
         <div>
           
